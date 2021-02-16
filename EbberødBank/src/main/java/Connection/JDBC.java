4f4b64e79a42e0a5f1dada8d8c2cbbf684c7d688
TheAgns <70578356+TheAgns@users.noreply.github.com>
2021-02-16 09:30:44 +0100

    Update JDBC.java

--- EbberødBank/src/main/java/Connection/JDBC.java
+++ EbberødBank/src/main/java/Connection/JDBC.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class JDBC {
     public static Connection getConnection() throws SQLException {
         String user = "root";
-        String password = "Agnsgaard1998!";
+        String password = "";
         String url = "jdbc:mysql://localhost:3306/bankData";
 
         Connection connection = null;
